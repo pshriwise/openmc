253909222fd35523d0f64b73659ee957031153dd
shriwise <pshriwise@gmail.com>
2018-08-11 08:34:37 -0500

    Simplifying index operators.

--- src/position.h
+++ src/position.h
@@ -20,20 +20,9 @@ struct Position {
   Position& operator-=(double);
   Position& operator*=(Position);
   Position& operator*=(double);
-  const double& operator[](int i) const {
-    switch (i) {
-      case 0: return x;
-      case 1: return y;
-      case 2: return z;
-    }
-  }
-  double& operator[](int i) {
-    switch (i) {
-      case 0: return x;
-      case 1: return y;
-      case 2: return z;
-    }
-  }
+  const double& operator[](int i) const { return xyz[i]; }
+  
+        double& operator[](int i)       { return xyz[i]; }
 
   // Other member functions
 
