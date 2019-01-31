e8544f1fb39465330490f57d61c875695863dedc
Patrick Shriwise <pshriwise@gmail.com>
2019-01-31 16:16:24 -0600

    Formalizing LevelCounter and CellCounter classes. Adding clear call in OpenMC finalize.

--- src/finalize.cpp
+++ src/finalize.cpp
@@ -132,6 +132,10 @@ int openmc_finalize()
 
 int openmc_reset()
 {
+
+  CellCountStorage::instance()->clear();
+  LevelCountStorage::instance()->clear();
+
   for (auto& t : model::tallies) {
     t->reset();
   }
