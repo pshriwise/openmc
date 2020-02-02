3ea9aedfc3cd4ce89d10283ff8345db6b92a4d1f
Patrick Shriwise <pshriwise@gmail.com>
2020-02-01 20:05:38 -0600

    Adding methods for transporting particles using delta tracking.

--- include/openmc/simulation.h
+++ include/openmc/simulation.h
@@ -97,6 +97,13 @@ void transport_history_based_single_particle(Particle& p);
 //! Simulate all particle histories using history-based parallelism
 void transport_history_based();
 
+//! Simulate a single particle history (and all generated secondary particles,
+//!  if enabled), from birth to death using delta tracking
+void transport_delta_tracking_single_particle(Particle& p);
+
+//! Simulate all particle histories using delta tracking
+void transport_delta_tracking();
+
 //! Simulate all particle histories using event-based parallelism
 void transport_event_based();
 
