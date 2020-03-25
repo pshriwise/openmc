c8f476620cbb2d5f86d49d9ded808a0dc6f6c279
Patrick Shriwise <pshriwise@gmail.com>
2020-03-25 00:18:30 -0500

    Some majorant functions.

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -16,6 +16,7 @@
 #include "openmc/error.h"
 #include "openmc/geometry_aux.h"
 #include "openmc/hdf5_interface.h"
+#include "openmc/majorant.h"
 #include "openmc/material.h"
 #include "openmc/memory.h"
 #include "openmc/message_passing.h"
