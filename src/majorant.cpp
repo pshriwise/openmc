38c474ae8788a2da94925d78fece0135276c9660
Patrick Shriwise <pshriwise@gmail.com>
2020-03-25 00:18:30 -0500

    Some majorant functions.

--- src/majorant.cpp
+++ src/majorant.cpp
@@ -17,4 +17,31 @@ namespace openmc {
     of.close();
   }
 
+  static std::pair<double, double>
+  intersect_2D(double x1, double y1,
+               double x2, double y2,
+               double x3, double y3,
+               double x4, double y4) {
+
+    double denominator = (x4 - x3) * (y1 - y2) - (x1 - x2) * (y4 - y3);
+
+    // if the lines are parallel, return no intersection
+    if (denominator == 0.0) { return {-1.0, -1.0}; }
+
+    double numerator = (y3 - y4) * (x1 - x3) + (x4 - x3) * (y1 - y3);
+
+    t = numerator / denominator;
+
+    if (0.0 <= t and t <= 1.0) {
+      double x = x1 + (x2 - x1) * t;
+      double m = (y2 - y1) / (x2 - x1);
+      double y = y1 + (x - x1) * m;
+
+      return {x, y};
+    }
+
+    return {-1, -1};
+  }
+
+
 }
