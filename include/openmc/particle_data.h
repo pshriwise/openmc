2e5d29b3a4a473b1b3dd86bc1cde57782b9748cf
Patrick Shriwise <pshriwise@gmail.com>
2020-04-14 02:27:17 -0500

    Implementing the new majorant. Needs some work still.

--- include/openmc/particle_data.h
+++ include/openmc/particle_data.h
@@ -538,6 +538,7 @@ private:
   int64_t n_progeny_ {0};
 
   bool delta_tracking_{false}; // !< Flag to indicate whether or not delta tracking is active
+  double majorant_ {0.0}; // !< most recent value for the majorant cross section
 
 public:
   //----------------------------------------------------------------------------
