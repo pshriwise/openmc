0eb2ee7ea8c2f587d80531ab5dd0e6b400bc13cf
Patrick Shriwise <pshriwise@gmail.com>
2020-05-07 11:18:52 -0500

    Further tweaks to the libmesh lib find.

--- cmake/Modules/FindLIBMESH.cmake
+++ cmake/Modules/FindLIBMESH.cmake
@@ -7,10 +7,7 @@ find_path(LIBMESH_PC NAMES libmesh-opt.pc
           PATH_SUFFIXES lib/pkgconfig pkgconfig
           NO_DEFAULT_PATH)
 
-set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH OFF)
-set(PKG_CONFIG_USE_CMAKE_ENVIRONMENT_PATH OFF)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${LIBMESH_PC}")
-message(STATUS "PKG_CONFIG_PATH: $ENV{PKG_CONFIG_PATH}")
 find_package(PkgConfig REQUIRED)
 
 pkg_check_modules(LIBMESH REQUIRED IMPORTED_TARGET libmesh)
@@ -18,17 +15,21 @@ pkg_check_modules(LIBMESH_DBG REQUIRED IMPORTED_TARGET libmesh-dbg)
 pkg_check_modules(LIBMESH_OPT REQUIRED IMPORTED_TARGET libmesh-opt)
 
 # older versions of CMake won't check the system libraries
-# if '-L' appears in the package config string. This
-# block makes sure the system locations are checked
+# if '-L' appears in the package config libraries string.
+# This makes sure the system locations are checked
 # if any of the libraries aren't found.
 get_cmake_property(ALL_VARIABLES VARIABLES)
 foreach(VARIABLE ${ALL_VARIABLES})
-  if (var MATCHES "^pkgcfg_lib_LIBMESH" AND ${var} MATCHES "NOTFOUND$")
+  if (NOT ${VARIABLE} AND VARIABLE MATCHES "^pkgcfg_lib_LIBMESH")
     # get the library name
-    string(REGEX REPLACE "^(.*[_])" "" LIBNAME ${var})
-    find_library(${var} NAMES ${LIBNAME})
+    string(REGEX REPLACE "^(.*[_])" "" LIBNAME ${VARIABLE})
+    find_library(${VARIABLE} NAMES ${LIBNAME})
+    if (NOT ${VARIABLE})
+      message(FATAL_ERROR "Could not find libMesh library: ${VARIABLE}.")
+    endif()
   endif()
 endforeach()
+
 
 
 message(STATUS "Found LIBMESH in ${LIBMESH_PC}")
