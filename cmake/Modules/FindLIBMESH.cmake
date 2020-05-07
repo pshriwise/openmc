5a45004ee7124419d3abbab7a21b89de97a450e2
Patrick Shriwise <pshriwise@gmail.com>
2020-05-06 21:11:28 -0500

    Updating libmesh configs used for various OpenMC build types (dbg, opt, etc.).
    Ensuring system library locations get checked for libmesh dependencies when using older versions of CMake.

--- cmake/Modules/FindLIBMESH.cmake
+++ cmake/Modules/FindLIBMESH.cmake
@@ -7,9 +7,28 @@ find_path(LIBMESH_PC NAMES libmesh-opt.pc
           PATH_SUFFIXES lib/pkgconfig pkgconfig
           NO_DEFAULT_PATH)
 
-include(FindPkgConfig)
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH OFF)
+set(PKG_CONFIG_USE_CMAKE_ENVIRONMENT_PATH OFF)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${LIBMESH_PC}")
-pkg_check_modules(LIBMESH REQUIRED libmesh IMPORTED_TARGET)
-pkg_check_modules(LIBMESH_DEVEL REQUIRED libmesh-devel IMPORTED_TARGET)
+message(STATUS "PKG_CONFIG_PATH: $ENV{PKG_CONFIG_PATH}")
+find_package(PkgConfig REQUIRED)
+
+pkg_check_modules(LIBMESH REQUIRED IMPORTED_TARGET libmesh)
+pkg_check_modules(LIBMESH_DBG REQUIRED IMPORTED_TARGET libmesh-dbg)
+pkg_check_modules(LIBMESH_OPT REQUIRED IMPORTED_TARGET libmesh-opt)
+
+# older versions of CMake won't check the system libraries
+# if '-L' appears in the package config string. This
+# block makes sure the system locations are checked
+# if any of the libraries aren't found.
+get_cmake_property(ALL_VARIABLES VARIABLES)
+foreach(VARIABLE ${ALL_VARIABLES})
+  if (var MATCHES "^pkgcfg_lib_LIBMESH" AND ${var} MATCHES "NOTFOUND$")
+    # get the library name
+    string(REGEX REPLACE "^(.*[_])" "" LIBNAME ${var})
+    find_library(${var} NAMES ${LIBNAME})
+  endif()
+endforeach()
+
 
 message(STATUS "Found LIBMESH in ${LIBMESH_PC}")
