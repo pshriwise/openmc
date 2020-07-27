5751efb1d67e70d953d3208050f5d4af635ce2da
Patrick Shriwise <pshriwise@gmail.com>
2020-07-26 23:48:11 -0500

    Adding a few more isotopes to the delta tracking test.

--- tests/regression_tests/delta_tracking/test.py
+++ tests/regression_tests/delta_tracking/test.py
@@ -15,14 +15,16 @@ def delta_tracking_model(nuclide):
     mat = openmc.Material()
     mat.add_nuclide(nuclide, 1.0)
     mat.set_density('g/cc', 19.1)
+    mat.depletable = True
 
     model.materials.append(mat)
 
     # geometry (infinite cell medium)
     cell = openmc.Cell()
     cell.fill = mat
+    cell.temperature = 300
 
-    prism = openmc.model.rectangular_prism(1000.00, 1000.00, boundary_type='reflective')
+    prism = openmc.model.rectangular_prism(1000.00, 1000.00, boundary_type='vacuum')
 
     cell.region = prism
 
@@ -33,11 +35,11 @@ def delta_tracking_model(nuclide):
     settings.particles = 100
     settings.batches = 10
     settings.inactive = 1
-    settings.resonance_scattering = {'enable' : False}
+    settings.delta_tracking = True
 
     return model
 
-nuclides = ('Fe56', 'U238', 'H1', 'O16')
+nuclides = ('Fe56', 'U238', 'H1', 'O16', 'He4', 'Zr91', 'Zr90', 'Zr92',  'Zr94',  'Zr96', 'B10', 'B11')
 @pytest.mark.parametrize('nuclide', nuclides)
 def test_delta_tracking(nuclide):
     model = delta_tracking_model(nuclide)
