b1feac5d58a05d2abf3e3a1008d8376650e4fb93
Patrick Shriwise <pshriwise@gmail.com>
2020-08-24 14:57:51 -0500

    Adding max methods to the Function1D classes.

--- include/openmc/endf.h
+++ include/openmc/endf.h
@@ -40,6 +40,8 @@ bool is_inelastic_scatter(int MT);
 class Function1D {
 public:
   virtual double operator()(double x) const = 0;
+  virtual double max() const = 0;
+  virtual double max(double min_E, double max_E) const = 0;
   virtual ~Function1D() = default;
 };
 
@@ -62,6 +64,9 @@ public:
   //! \return Polynomial evaluated at x
   double operator()(double x) const override;
 
+  double max() const override;
+  double max(double min_e, double max_E) const override;
+
 private:
   vector<double> coef_; //!< Polynomial coefficients
 };
@@ -82,6 +87,9 @@ public:
   //! \param[in] x independent variable
   //! \return Function evaluated at x
   double operator()(double x) const override;
+
+  double max() const override;
+  double max(double min_E, double max_E) const override;
 
   // Accessors
   const vector<double>& x() const { return x_; }
@@ -106,6 +114,9 @@ public:
 
   double operator()(double E) const override;
 
+  double max() const override;
+  double max(double min_E, double max_E) const override;
+
   const vector<double>& bragg_edges() const { return bragg_edges_; }
   const vector<double>& factors() const { return factors_; }
 
@@ -123,6 +134,9 @@ public:
   explicit IncoherentElasticXS(hid_t dset);
 
   double operator()(double E) const override;
+
+  double max() const override;
+  double max(double min_E, double max_E) const override;
 
 private:
   double bound_xs_; //!< Characteristic bound xs in [b]
