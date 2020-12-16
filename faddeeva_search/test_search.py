8f2f3d679289afcfbbfe9de8002c9d42cfba3f67
Patrick Shriwise <pshriwise@gmail.com>
2020-12-15 23:19:08 -0600

    Checking all nuclides in the library.

--- faddeeva_search/test_search.py
+++ faddeeva_search/test_search.py
@@ -6,15 +6,27 @@ import pytest
 
 current_dir = Path(__file__).parent
 
-datasets = ('endfb_71', 'endfb_80')
+datasets = ('endfb71_hdf5', 'endfb80_hdf5')
 
-def test_inf_media():
+# create a library from the cross sections XML (just ENDF/B 7.1 for now)
+xs_xml = current_dir / datasets[0] / 'cross_sections.xml'
+data_lib = openmc.data.DataLibrary.from_xml(xs_xml)
+
+
+nuclides = []
+for lib in data_lib.libraries:
+    if lib['type'] in ('wmp', 'neutron'):
+        nuclides.append(lib['materials'][0])
+
+
+@pytest.mark.parametrize('nuclide', nuclides)
+def test_inf_media(nuclide):
     # setup and infinite media problem
     model = openmc.model.Model()
 
     # create a material
     mat = openmc.Material()
-    mat.add_nuclide('O16', 1.0, 'ao')
+    mat.add_nuclide(nuclide, 1.0, 'ao')
 
     cell = openmc.Cell(fill=mat)
     cell.region = openmc.rectangular_prism(1E5, 1E5, boundary_type='reflective')
@@ -25,11 +37,10 @@ def test_inf_media():
     model.geometry = openmc.Geometry([cell])
 
     model.settings.run_mode = 'fixed source'
-    model.settings.particles = 10
+    model.settings.particles = 1000
     model.settings.batches = 10
     model.settings.temperature['multipole'] = True
     model.settings.resonance_scattering['enable'] = True
-
 
     model.export_to_xml()
     openmc.run()
