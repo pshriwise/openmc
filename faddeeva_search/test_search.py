7f9ceea7ba952392cf16f911e962304187d6f345
Patrick Shriwise <pshriwise@gmail.com>
2020-12-15 23:39:34 -0600

    Adding mono-energetic source (10MeV) at the origin.

--- faddeeva_search/test_search.py
+++ faddeeva_search/test_search.py
@@ -32,7 +32,8 @@ def test_inf_media(nuclide):
     cell.region = openmc.rectangular_prism(1E5, 1E5, boundary_type='reflective')
 
     model.materials = [mat]
-    model.materials.cross_sections = str(current_dir / "endfb71_hdf5" / "cross_sections.xml")
+    xs_dir = str(current_dir / "endfb71_hdf5" / "cross_sections.xml")
+    model.materials.cross_sections = xs_dir
 
     model.geometry = openmc.Geometry([cell])
 
@@ -40,7 +41,16 @@ def test_inf_media(nuclide):
     model.settings.particles = 1000
     model.settings.batches = 10
     model.settings.temperature['multipole'] = True
+    model.settings.temperature['default'] = 294.0
     model.settings.resonance_scattering['enable'] = True
+
+
+    src_loc = openmc.stats.Point()
+    src_e = openmc.stats.Discrete([10E6], [1.0])
+    model.settings.source = openmc.Source(space=src_loc, energy=src_e)
 
     model.export_to_xml()
     openmc.run()
+
+if __name__ == '__main__':
+    test_inf_media('H1')
