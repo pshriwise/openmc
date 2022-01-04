960c710f8847c6a0f2b796b096bfe08146459cf3
Patrick Shriwise <pshriwise@gmail.com>
2022-01-04 13:43:38 -0600

    Adding decorator for running a function on a single rank with a barrier.

--- openmc/mpi.py
+++ openmc/mpi.py
@@ -6,3 +6,22 @@ except ImportError:
     MPI = Mock()
     from openmc.dummy_comm import DummyCommunicator
     comm = DummyCommunicator()
+
+
+def with_barrier(rank=0):
+    """
+    Decorator indicating that a function should always run
+    on a specified rank with a barrier for all other procs
+
+    func : Callable
+        The function to be called
+    rank : int
+        Process rank that will do the work (default 0)
+    """
+    def dec(func):
+        def wrapper(*args, **kwargs):
+            if comm.rank == rank:
+                func(*args, **kwargs)
+            comm.barrier()
+        return wrapper
+    return dec
