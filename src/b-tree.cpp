1b78e289efe56d499e2e897495f0b60269403353
Patrick Shriwise <pshriwise@gmail.com>
2022-05-04 23:11:14 -0500

    Trying a simpler b-tree implementation

--- src/b-tree.cpp
+++ src/b-tree.cpp
@@ -1,9 +1,6 @@
-// clang++ -O3 -std=c++17 -march=native standalone.cc
-// GCC also compiles, but the performance is slightly worse
-// Requires an x86 CPU with at least AVX2
 
-// On Linux, make sure madvise is enabled to use hugepages
-// (https://en.algorithmica.org/hpc/cpu-cache/paging/#changing-page-size)
+#include <x86intrin.h>
+#include <limits>
 
 #pragma GCC optimize("O3")
 #pragma GCC target("avx2,bmi")
@@ -12,161 +9,108 @@
 #include <x86intrin.h>
 #include <sys/mman.h>
 
-// Windows:
-// #include "memoryapi.h"
+#ifdef BTREE_BENCHMARK
+#include <bits/stdc++.h>
 
-#define _CMP_GT_OQ    0x1e
+#ifndef N
+#define N (1<<20)
+#endif
 
-typedef __m256i reg;
-typedef __m256d dreg;
+void prepare(int *a, int n);
+int lower_bound(int x);
 
-const int N = (1<<16), Q = (1<<22); // <- change these
+int main(int argc, char* argv[]) {
+    int n = (argc > 1 ? atoi(argv[1]) : N);
+    int m = (argc > 2 ? atoi(argv[2]) : 1<<20);
 
-const int B = 16;
-const double INF = std::numeric_limits<double>::max();
+    int *a = new int[n];
+    int *q = new int[m];
 
-constexpr int blocks(int n) {
-    return (n + B - 1) / B;
-}
+    for (int i = 0; i < n; i++)
+        a[i] = rand();
+    for (int i = 0; i < m; i++)
+        q[i] = rand();
 
-constexpr int prev_keys(int n) {
-    return (blocks(n) + B) / (B + 1) * B;
-}
+    a[0] = RAND_MAX;
+    std::sort(a, a + n);
 
-constexpr int height(int n) {
-    return (n <= B ? 1 : height(prev_keys(n)) + 1);
-}
+    prepare(a, n);
 
-constexpr int offset(int h) {
-    int k = 0, n = N;
-    while (h--) {
-        k += blocks(n) * B;
-        n = prev_keys(n);
+    int checksum = 0;
+    clock_t start = clock();
+
+    #ifdef LATENCY
+    int last = 0;
+
+    for (int i = 0; i < m; i++) {
+        last = lower_bound(q[i] ^ last);
+        checksum ^= last;
     }
-    return k;
-}
-
-const int H = height(N), S = offset(H);
-
-double *btree;
-
-void permute(double *node) {
-    const reg perm_mask = _mm256_set_epi32(3, 2, 1, 0, 7, 6, 5, 4);
-    double* middle = node + 4;
-    dreg x = _mm256_loadu_pd(middle);
-    x = _mm256_permutevar_pd(x, perm_mask);
-    _mm256_storeu_pd(middle, x);
-}
-
-void prepare(double *a) {
-    const int P = 1 << 21, T = (4 * S + P - 1) / P * P;
-    btree = (double*) aligned_alloc(P, T);
-    #ifdef __linux__
-    madvise(btree, T, MADV_HUGEPAGE);
+    #else
+    for (int i = 0; i < m; i++)
+        checksum ^= lower_bound(q[i]);
     #endif
 
-    for (int i = N; i < S; i++)
-        btree[i] = INF;
+    float seconds = float(clock() - start) / CLOCKS_PER_SEC;
 
-    memcpy(btree, a, 4 * N);
+    printf("%.2f ns per query\n", 1e9 * seconds / m);
+    printf("%d\n", checksum);
 
-    for (int h = 1; h < H; h++) {
-        for (int i = 0; i < offset(h + 1) - offset(h); i++) {
-            int k = i / B,
-                j = i - k * B;
-            k = k * (B + 1) + j + 1;
-            for (int l = 0; l < h - 1; l++)
-                k *= (B + 1);
-            btree[offset(h) + i] = (k * B < N ? btree[k * B] : INF);
+    return 0;
+}
+
+#endif
+
+typedef __m256i reg;
+
+const int B = 16;
+const int INF = std::numeric_limits<int>::max();
+
+int n;
+int nblocks;
+int *_a;
+int (*btree)[B];
+
+int go(int k, int i) { return k * (B + 1) + i + 1; }
+
+void build(int k = 0) {
+    static int t = 0;
+    if (k < nblocks) {
+        for (int i = 0; i < B; i++) {
+            build(go(k, i));
+            btree[k][i] = (t < n ? _a[t++] : INF);
         }
+        build(go(k, B));
     }
-
-    for (int i = offset(1); i < S; i += B)
-        permute(btree + i);
 }
 
-unsigned direct_rank(dreg x, double* y) {
-    dreg a = _mm256_load_pd(y);
-    dreg b = _mm256_load_pd(y + 8);
-
-    dreg ca = _mm256_cmp_pd(a, x, _CMP_GT_OQ);
-    dreg cb = _mm256_cmp_pd(b, x, _CMP_GT_OQ);
-
-    int mb = _mm256_movemask_pd(cb);
-    int ma = _mm256_movemask_pd(ca);
-
-    unsigned mask = (1 << 16);
-    mask |= mb << 8;
-    mask |= ma;
-
-    return __tzcnt_u32(mask);
+void prepare(int *a, int _n) {
+    n = _n;
+    nblocks = (n + B - 1) / B;
+    _a = a;
+    btree = (int(*)[16]) aligned_alloc(64, 64 * nblocks);
+    build();
 }
 
-unsigned permuted_rank(dreg x, double* y) {
-    dreg a = _mm256_load_pd(y);
-    dreg b = _mm256_load_pd(y + 8);
-
-    dreg ca = _mm256_cmp_pd(a, x, _CMP_GT_OQ);
-    dreg cb = _mm256_cmp_pd(b, x, _CMP_GT_OQ);
-
-    dreg c = (dreg) _mm256_packs_epi32((reg)ca, (reg)cb);
-    unsigned mask = _mm256_movemask_pd(c);
-
-    return __tzcnt_u32(mask);
+int cmp(reg x_vec, int* y_ptr) {
+    reg y_vec = _mm256_load_si256((reg*) y_ptr);
+    reg mask = _mm256_cmpgt_epi32(x_vec, y_vec);
+    return _mm256_movemask_ps((__m256) mask);
 }
 
-int lower_bound(double _x) {
-    unsigned k = 0;
-    dreg x = _mm256_set1_pd(_x - 1);
-    for (int h = H - 1; h > 0; h--) {
-        unsigned i = permuted_rank(x, btree + offset(h) + k);
-        k = k * (B + 1) + (i << 3);
+int lower_bound(int x) {
+    int k = 0, res = INF;
+    reg x_vec = _mm256_set1_epi32(x);
+    while (k < nblocks) {
+        int mask = ~(
+            cmp(x_vec, &btree[k][0]) +
+            (cmp(x_vec, &btree[k][8]) << 8)
+        );
+        int i = __builtin_ffs(mask) - 1;
+        if (i < B)
+            res = btree[k][i];
+        k = go(k, i);
     }
-    unsigned i = direct_rank(x, btree + k);
-    return btree[k + i];
+    return res;
 }
 
-int a[N], q[Q];
-
-// int baseline(int x) {
-//     return *std::lower_bound(a, a + N, x);
-// }
-
-// double timeit(int (*f)(int)) {
-//     clock_t start = clock();
-
-//     int checksum = 0;
-
-//     for (int i = 0; i < Q; i++)
-//         checksum ^= f(q[i]);
-
-//     double seconds = double(clock() - start) / CLOCKS_PER_SEC;
-//     printf("Checksum: %d\n", checksum);
-
-//     return 1e9 * seconds / Q;
-// }
-
-// int main() {
-//     printf("N = %d, Q = %d\n", N, Q);
-
-//     std::mt19937 rng(0);
-
-//     for (int i = 0; i < N; i++)
-//         a[i] = rng() % (1 << 30);
-//     for (int i = 0; i < Q; i++)
-//         q[i] = rng() % (1 << 30);
-
-//     a[0] = INF;
-//     std::sort(a, a + N);
-//     prepare(a);
-
-//     double x = timeit(baseline);
-//     double y = timeit(lower_bound);
-
-//     printf("std::lower_bound: %.2f\n", x);
-//     printf("S+ tree: %.2f\n", y);
-
-//     printf("Speedup: %.2f\n", x / y);
-
-//     return 0;
-// }
