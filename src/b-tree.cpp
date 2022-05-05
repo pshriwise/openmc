62c34790f8432c12affe83e09807047015c2cfa0
Patrick Shriwise <pshriwise@gmail.com>
2022-05-05 15:57:19 -0500

    Data collection version. Not as fast?

--- src/b-tree.cpp
+++ src/b-tree.cpp
@@ -17,27 +17,25 @@ const int B = 8;
 const double INF = std::numeric_limits<double>::max();
 
 // state variables
-const int N = (1<<20);
+int N = (1<<20);
+
 int nblocks;
 double *_a;
-// double (*btree)[B];
 double *btree;
 
-// constants of the tree
-
-constexpr int blocks(int n) {
+int blocks(int n) {
     return (n + B - 1) / B;
 }
 
-constexpr int prev_keys(int n) {
+int prev_keys(int n) {
     return (blocks(n) + B) / (B + 1) * B;
 }
 
-constexpr int height(int n) {
+int height(int n) {
     return (n <= B ? 1 : height(prev_keys(n)) + 1);
 }
 
-constexpr int offset(int h) {
+int offset(int h) {
     int k = 0, n = N;
     while (h--) {
         k += blocks(n) * B;
@@ -46,20 +44,7 @@ constexpr int offset(int h) {
     return k;
 }
 
-const int H = height(N), S = offset(H);
-
-int go(int k, int i) { return k * (B + 1) + i + 1; }
-
-// void build(int k = 0) {
-//     static int t = 0;
-//     if (k < nblocks) {
-//         for (int i = 0; i < B; i++) {
-//             build(go(k, i));
-//             btree[k][i] = (t < N ? _a[t++] : INF);
-//         }
-//         build(go(k, B));
-//     }
-// }
+int H = height(N), S = offset(H);
 
 void prepare(double *a, int _n) {
     const int P = 1 << 21, T = (8 * S + P - 1) / P * P;
@@ -82,12 +67,6 @@ void prepare(double *a, int _n) {
             btree[offset(h) + i] = (k * B < N ? btree[k * B] : INF);
         }
     }
-}
-
-int cmp(dreg x_vec, double* y_ptr) {
-    dreg y_vec = _mm256_load_pd(y_ptr);
-    dreg mask = _mm256_cmp_pd(x_vec, y_vec, _CMP_GT_OQ);
-    return _mm256_movemask_pd(mask);
 }
 
 int direct_rank(dreg x, double* y_ptr) {
@@ -114,26 +93,28 @@ double lower_bound(double _x) {
 }
 
 #ifdef BTREE_BENCHMARK
-#include <bits/stdc++.h>
-
 
 int main(int argc, char* argv[]) {
     int n = (argc > 1 ? atoi(argv[1]) : N);
     int m = (argc > 2 ? atoi(argv[2]) : 1<<20);
 
+    N = n;
+    H = height(N);
+    S = offset(H);
+
     double *a = new double[n];
     double *q = new double[m];
 
-    std::cout << "Array size: " << n << std::endl;
+    // std::cout << "Array size: " << n << std::endl;
     for (int i = 0; i < n; i++)
         a[i] = (double)rand() / (double)RAND_MAX;
 
-    std::cout << "# Queries: " << m << std::endl;
+    // std::cout << "# Queries: " << m << std::endl;
     for (int i = 0; i < m; i++) {
         q[i] = (double)rand() / (double)RAND_MAX;
     }
 
-    std::cout << "Tree height: " << H << std::endl;
+    // std::cout << "Tree height: " << H << std::endl;
 
     std::cout << std::endl;
 
@@ -147,19 +128,20 @@ int main(int argc, char* argv[]) {
 
     for (int i = 0; i < m; i++) {
         // std::cout << "Query: " << q[i] << std::endl;
-        double result = lower_bound(q[i]);
-        checksum ^= (int)result;
+        // double result = lower_bound(q[i]);
         // std::cout << "Result: " << result << std::endl;
+        checksum ^= (int)lower_bound(q[i]);
     }
 
-    float seconds = float(clock() - start) / CLOCKS_PER_SEC;
+    float b_seconds =  float(clock() - start) / CLOCKS_PER_SEC;
 
-    std::cout << "std::lower_bound" << std::endl;
-    std::cout << "--------" << std::endl;
-    printf("%.2f ns per query\n", 1e9 * seconds / m);
-    std::cout << "Checksum: " << checksum << std::endl;
-    std::cout << "--------" << std::endl;
-    std::cout << std::endl;
+
+    // std::cout << "B+ Tree" << std::endl;
+    // std::cout << "--------" << std::endl;
+    // printf("%.2f ns per query\n", 1e9 * b_seconds / m);
+    std::cerr << "Checksum: " << checksum << std::endl;
+    // std::cout << "--------" << std::endl;
+    // std::cout << std::endl;
 
     checksum = 0;
     start = clock();
@@ -168,13 +150,18 @@ int main(int argc, char* argv[]) {
         checksum ^= (int)*std::lower_bound(a, a+n, q[i]);
     }
 
-    seconds = float(clock() - start) / CLOCKS_PER_SEC;
+    float std_seconds = float(clock() - start) / CLOCKS_PER_SEC;
 
-    std::cout << "B+ Tree" << std::endl;
-    std::cout << "-------" << std::endl;
-    printf("%.2f ns per query\n", 1e9 * seconds / m);
-    std::cout << "Checksum: " << checksum << std::endl;
-    std::cout << "-------" << std::endl;
+    // std::cout << "std::lower_bound" << std::endl;
+    // std::cout << "-------" << std::endl;
+    // printf("%.2f ns per query\n", 1e9 * std_seconds / m);
+    // std::cout << "Checksum: " << checksum << std::endl;
+    // std::cout << "-------" << std::endl;
+
+    // std::cout << std::endl;
+    // std::cout << "Speedup: " << std_seconds / b_seconds << std::endl;
+
+    std::cout << n << ", " << 1e9 * b_seconds / m << ", " << 1e9 * std_seconds / m << ", " << checksum << std::endl;
 
     return 0;
 }
