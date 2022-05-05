3ed67bc9bfa300593b1b589e601b4678a9a1202d
Patrick Shriwise <pshriwise@gmail.com>
2022-05-05 09:26:42 -0500

    Flip-flop code for my tiny brain

--- src/b-tree.cpp
+++ src/b-tree.cpp
@@ -10,61 +10,6 @@
 #include <x86intrin.h>
 #include <sys/mman.h>
 
-#ifdef BTREE_BENCHMARK
-#include <bits/stdc++.h>
-
-#ifndef N
-#define N (1<<20)
-#endif
-
-void prepare(double *a, int n);
-double lower_bound(double x);
-
-int main(int argc, char* argv[]) {
-    int n = (argc > 1 ? atoi(argv[1]) : N);
-    int m = (argc > 2 ? atoi(argv[2]) : 1<<20);
-
-    double *a = new double[n];
-    double *q = new double[m];
-
-    std::cout << "Array size: " << n << std::endl;
-    for (int i = 0; i < n; i++)
-        a[i] = (double)rand() / (double)RAND_MAX;
-
-    std::cout << "Queries: " << m << std::endl;
-    for (int i = 0; i < m; i++) {
-        q[i] = (double)rand() / (double)RAND_MAX;
-        //std::cout << "Query val: " << q[i] << std::endl;
-    }
-
-
-    a[0] = RAND_MAX;
-    std::sort(a, a + n);
-
-    prepare(a, n);
-
-    int checksum = 0;
-    clock_t start = clock();
-
-    for (int i = 0; i < m; i++) {
-        // std::cout << i << std::endl;
-        // std::cout << "Query: " << q[i] << std::endl;
-        // double result = lower_bound(q[i]);
-        // std::cout << "Result: " << result << std::endl;
-        checksum ^= (int)lower_bound(q[i]);
-        // checksum ^= (int)*std::lower_bound(a, a+n, q[i]);
-    }
-
-    float seconds = float(clock() - start) / CLOCKS_PER_SEC;
-
-    printf("%.2f ns per query\n", 1e9 * seconds / m);
-    std::cout << "Checksum: " << checksum << std::endl;
-
-    return 0;
-}
-
-#endif
-
 typedef __m256i reg;
 typedef __m256d dreg;
 
@@ -119,4 +64,57 @@ double lower_bound(double x) {
     }
     return res;
 }
+
+
+#ifdef BTREE_BENCHMARK
+#include <bits/stdc++.h>
+
+#ifndef N
+#define N (1<<20)
+#endif
+
+int main(int argc, char* argv[]) {
+    int n = (argc > 1 ? atoi(argv[1]) : N);
+    int m = (argc > 2 ? atoi(argv[2]) : 1<<20);
+
+    double *a = new double[n];
+    double *q = new double[m];
+
+    std::cout << "Array size: " << n << std::endl;
+    for (int i = 0; i < n; i++)
+        a[i] = (double)rand() / (double)RAND_MAX;
+
+    std::cout << "Queries: " << m << std::endl;
+    for (int i = 0; i < m; i++) {
+        q[i] = (double)rand() / (double)RAND_MAX;
+        //std::cout << "Query val: " << q[i] << std::endl;
+    }
+
+
+    a[0] = RAND_MAX;
+    std::sort(a, a + n);
+
+    prepare(a, n);
+
+    int checksum = 0;
+    clock_t start = clock();
+
+    for (int i = 0; i < m; i++) {
+        // std::cout << i << std::endl;
+        // std::cout << "Query: " << q[i] << std::endl;
+        // double result = lower_bound(q[i]);
+        // std::cout << "Result: " << result << std::endl;
+        checksum ^= (int)lower_bound(q[i]);
+        // checksum ^= (int)*std::lower_bound(a, a+n, q[i]);
+    }
+
+    float seconds = float(clock() - start) / CLOCKS_PER_SEC;
+
+    printf("%.2f ns per query\n", 1e9 * seconds / m);
+    std::cout << "Checksum: " << checksum << std::endl;
+
+    return 0;
+}
+
+#endif
 
