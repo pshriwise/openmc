813883f1f9a8109946809ebea411586992f9c10f
Patrick Shriwise <pshriwise@gmail.com>
2022-05-16 08:31:11 -0500

    Correction to cross product

--- include/openmc/position.h
+++ include/openmc/position.h
@@ -86,7 +86,7 @@ struct Position {
   //! \param[in] other Vector to take cross product with
   //! \result Resulting cross product
   inline Position cross(Position other) const {
-    return {y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.z};
+    return {y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x};
   }
 
   inline double norm() const { return std::sqrt(x * x + y * y + z * z); }
