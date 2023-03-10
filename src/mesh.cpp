832bc4240e8389f38f4ff91c6e26ec7d64794ba5
Patrick Shriwise <pshriwise@gmail.com>
2023-03-09 22:32:09 -0600

    Adding fix for cylindrical and spherical mesh crossings

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1143,10 +1143,13 @@ StructuredMesh::MeshDistance CylindricalMesh::distance_to_grid_boundary(
 {
 
   if (i == 0) {
-
-    return std::min(
-      MeshDistance(ijk[i] + 1, true, find_r_crossing(r0, u, l, ijk[i])),
-      MeshDistance(ijk[i] - 1, false, find_r_crossing(r0, u, l, ijk[i] - 1)));
+    // create temporary position to determine which crossing to select
+    if ((r0.x * r0.x + r0.y * r0.y) == 0.0 || (r0.x * u.x + r0.y * u.y) > 0.0) {
+      return MeshDistance(ijk[i] + 1, true, find_r_crossing(r0, u, l, ijk[i]));
+    } else {
+      return MeshDistance(
+        ijk[i] - 1, false, find_r_crossing(r0, u, l, ijk[i] - 1));
+    }
 
   } else if (i == 1) {
 
@@ -1402,13 +1405,11 @@ StructuredMesh::MeshDistance SphericalMesh::distance_to_grid_boundary(
   const MeshIndex& ijk, int i, const Position& r0, const Direction& u,
   double l) const
 {
-
   if (i == 0) {
     // return the distance for the next surface ahead of the particle depending
     // on its direction. If the particle is at the origin, always intersect
     // next largest surface in the mesh
-    double norm = r0.norm();
-    if (norm == 0.0 || (r0 / norm).dot(u) > 0) {
+    if (r0.norm() == 0.0 || r0.dot(u) > 0.0) {
       return MeshDistance(ijk[i] + 1, true, find_r_crossing(r0, u, l, ijk[i]));
     } else {
       return MeshDistance(
