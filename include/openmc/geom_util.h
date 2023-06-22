45aab39645b40fdd3feb70d441d9ec9413fac237
Patrick Shriwise <pshriwise@gmail.com>
2023-06-22 15:33:26 -0500

    Moving ray triangle intersection into its own file

---
+++ include/openmc/geom_util.h
@@ -0,0 +1,20 @@
+#ifndef OPENMC_GEOM_UTIL_H
+#define OPENMC_GEOM_UTIL_H
+
+#include <array>
+
+#include "openmc/position.h"
+
+namespace openmc {
+
+bool plucker_ray_tri_intersect(const std::array<Position, 3> vertices,
+                               const Position& origin,
+                               const Position& direction,
+                               double& dist_out,
+                               const double* neg_ray_len = nullptr,
+                               const int* orientation = nullptr);
+
+}
+
+
+#endif
