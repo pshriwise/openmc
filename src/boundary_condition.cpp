30b6be6450f531ad9e5d442788cac28d3cc57d3f
Gavin Ridley <gavin.keith.ridley@gmail.com>
2023-08-18 14:03:15 -0400

    factor particle geometry state out into Geometron

--- src/boundary_condition.cpp
+++ src/boundary_condition.cpp
@@ -25,7 +25,7 @@ void VacuumBC::handle_particle(Particle& p, const Surface& surf) const
 
 void ReflectiveBC::handle_particle(Particle& p, const Surface& surf) const
 {
-  Direction u = surf.reflect(p.r(), p.u(), &p);
+  Direction u = surf.reflect(p.r(), p.u());
   u /= u.norm();
 
   p.cross_reflective_bc(surf, u);
