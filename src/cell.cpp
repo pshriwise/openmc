a7a122b46c5fb1814b0f9ecd9980cf5fdf5cb6cf
Patrick Shriwise <pshriwise@gmail.com>
2024-03-13 22:33:12 -0500

    Read XDG universes as well

--- src/cell.cpp
+++ src/cell.cpp
@@ -22,6 +22,7 @@
 #include "openmc/material.h"
 #include "openmc/nuclide.h"
 #include "openmc/settings.h"
+#include "openmc/xdg.h"
 #include "openmc/xml_interface.h"
 
 namespace openmc {
@@ -1003,7 +1004,9 @@ void read_cells(pugi::xml_node node)
     }
   }
 
+  // read CAD-based geometry universes
   read_dagmc_universes(node);
+  read_xdg_universes(node);
 
   populate_universes();
 
