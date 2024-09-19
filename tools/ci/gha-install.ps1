e8a989088b8ace0860758382980b82900338dc01
Patrick Shriwise <pshriwise@gmail.com>
2024-09-19 12:37:47 -0500

    Update gha-install.ps1

--- tools/ci/gha-install.ps1
+++ tools/ci/gha-install.ps1
@@ -1,3 +1,21 @@
+$ErrorActionPreference = "Stop"
+
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        if ($CleanupScript) {
+            "Executing cleanup script: $CleanupScript"
+            &$CleanupScript
+        }
+        $msg = @"
+EXE RETURNED EXIT CODE $LastExitCode
+CALLSTACK:$(Get-PSCallStack | Out-String)
+"@
+        throw $msg
+    }
+}
+
 # Upgrade pip, pytest, numpy before doing anything else.
 pip install --upgrade pip
 pip install --upgrade pytest
@@ -5,6 +23,7 @@ pip install --upgrade numpy
 
 # Build and install OpenMC executable
 python tools/ci/gha-install.py
+CheckLastExitCode
 
 # Install Python API in editable mode
 pip install -e .[test,vtk,ci]
