890ddd6f008c8eae4a406853a57bc913980ade53
Patrick Shriwise <pshriwise@gmail.com>
2024-11-26 12:51:58 -0600

    Reset bank counter when reviving from secondary

--- src/particle.cpp
+++ src/particle.cpp
@@ -417,6 +417,8 @@ void Particle::event_revive_from_secondary()
     from_source(&secondary_bank().back());
     secondary_bank().pop_back();
     n_event() = 0;
+    
+    n_bank_second() = 0;
 
     // Subtract secondary particle energy from interim pulse-height results
     if (!model::active_pulse_height_tallies.empty() &&
