94ee9012104faeceb451909688b44cdcf39667bc
Patrick Shriwise <pshriwise@gmail.com>
2025-01-14 14:25:37 -0600

    New method to update the camera to model matrix

--- src/plot.cpp
+++ src/plot.cpp
@@ -1052,7 +1052,11 @@ RayTracePlot::RayTracePlot(pugi::xml_node node) : PlottableInterface(node)
     fatal_error("orthographic_width and field_of_view are mutually exclusive "
                 "parameters.");
 
-  // Get centerline vector for camera-to-model. We create vectors around this
+
+}
+
+void RayTracePlot::update_view() {
+    // Get centerline vector for camera-to-model. We create vectors around this
   // that form a pixel array, and then trace rays along that.
   auto up = up_ / up_.norm();
   Direction looking_direction = look_at_ - camera_position_;
@@ -1526,8 +1530,9 @@ void PhongPlot::print_info() const
   RayTracePlot::print_info();
 }
 
-ImageData PhongPlot::create_image() const
+ImageData PhongPlot::create_image()
 {
+  update_view();
   size_t width = pixels_[0];
   size_t height = pixels_[1];
   ImageData data({width, height}, not_found_);
@@ -1549,7 +1554,7 @@ ImageData PhongPlot::create_image() const
 void PhongPlot::create_output() const
 {
 
-  ImageData data = create_image();
+  ImageData data; // = create_image();
 
 #ifdef USE_LIBPNG
   output_png(path_plot(), data);
