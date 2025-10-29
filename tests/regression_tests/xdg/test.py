fe6d5d95eb7313f315f36737fbb54e35e9ef758e
Patrick Shriwise <pshriwise@gmail.com>
2025-10-28 22:41:30 -0500

    Expose XDG capability variable through the CAPI for use in the test suite.

--- tests/regression_tests/xdg/test.py
+++ tests/regression_tests/xdg/test.py
@@ -10,8 +10,8 @@ import pytest
 from tests.testing_harness import PyAPITestHarness, config
 
 pytestmark = pytest.mark.skipif(
-    not openmc.lib._dagmc_enabled(),
-    reason="DAGMC CAD geometry is not enabled.")
+    not openmc.lib._xdg_enabled(),
+    reason="XDG geometry is not enabled.")
 
 @pytest.fixture
 def model():
